bf60d16c4b7297aa25fa34a8b29ba0e834a7de7c
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-11-22 04:48:12 -0800

    Fix Movie list

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -21,6 +21,9 @@ const Movies = () => {
     page,
     searchQuery,
   });
+  const lg = useMediaQuery((theme) => theme.breakpoints.only("lg"));
+
+  const numberOfMovies = lg ? 16 : 18;
 
   if (isFetching) {
     return (
@@ -46,7 +49,7 @@ const Movies = () => {
 
   return (
     <div>
-      <MovieList movies={data} numberOfMovies={18} />
+      <MovieList movies={data} numberOfMovies={numberOfMovies} />
       <Pagination
         currentPage={page}
         setPage={setPage}
