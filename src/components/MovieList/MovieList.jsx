f4afb381fea99b7c8b988d2e5d707ded7fde9cbc
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-11-22 10:46:41 -0800

    Add FeaturedMovies Card

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import { Grid } from '@mui/material';
+import React from "react";
+import { Grid } from "@mui/material";
 
-import useStyles from './styles';
-import { Movie } from '..';
+import useStyles from "./styles";
+import { Movie } from "..";
 
-const MovieList = ({ movies, numberOfMovies }) => {
+const MovieList = ({ movies, numberOfMovies, excludeFirst }) => {
   const classes = useStyles();
+  const startFrom = excludeFirst ? 1 : 0;
 
   return (
     <Grid container className={classes.moviesContainer}>
-      {movies.results.slice(0, numberOfMovies).map((movie, i) => (
+      {movies.results.slice(startFrom, numberOfMovies).map((movie, i) => (
         <Movie key={i} movie={movie} i={i} />
       ))}
     </Grid>
